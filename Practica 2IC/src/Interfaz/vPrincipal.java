b411ef36c6de13d79c5637a26a67adf62a4fa880
smarcitllach <smarci01@ucm.es>
2023-04-03 19:52:13 +0200

    keloke

---
+++ Practica 2IC/src/Interfaz/vPrincipal.java
@@ -0,0 +1,97 @@
+package Interfaz;
+
+import javax.swing.*;
+
+import Modelo.LecturaDatos;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class vPrincipal extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	public vPrincipal(){
+	    initGUI();
+	}
+	
+	public void initGUI() {
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		JButton cargarAtrib = new JButton("Cargar Atributos");
+		JButton cargarEjemp = new JButton("Cargar Ejemplos");
+		JButton botonCalcular = new JButton("Calcular");
+		JLabel solLabel = new JLabel("Solución: ");
+		JTextField solTB = new JTextField();
+		
+		cargarAtrib.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				fc.setCurrentDirectory(new File(new File("./resources/examples/").getAbsolutePath()));
+				
+				int op = fc.showOpenDialog(fc);
+				if(op == fc.APPROVE_OPTION) {
+					try {
+						File fichero = fc.getSelectedFile();
+						LecturaDatos.readAtributos(fichero);
+						
+					}catch(Exception ex) {
+						JOptionPane.showMessageDialog(null, "ERROR AL CARGAR LOS ATRIBUTOS", "ERROR", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		});
+		
+		cargarEjemp.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				fc.setCurrentDirectory(new File(new File("./resources/examples/").getAbsolutePath()));
+				
+				int op = fc.showOpenDialog(fc);
+				if(op == fc.APPROVE_OPTION) {
+					try {
+						File fichero = fc.getSelectedFile();
+						LecturaDatos.readEjemplos(fichero);
+						
+					}catch(Exception ex) {
+						JOptionPane.showMessageDialog(null, "ERROR AL CARGAR LOS EJEMPLOS", "ERROR", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		});
+		
+		mainPanel.setLayout(new GridLayout(1,2));
+		mainPanel.add(cargarAtrib);
+		mainPanel.add(cargarEjemp);
+		mainPanel.add(botonCalcular);
+		//mainPanel.add(solLabel);
+		//mainPanel.add(solTB);
+		
+		this.setTitle("ID3");
+		this.setResizable(false);
+		this.setMinimumSize(new Dimension(900,300));
+		this.setLocationRelativeTo(null);
+		this.add(mainPanel);
+		this.setVisible(true);
+		this.pack();
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vPrincipal frame = new vPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+}
---
+++ Practica 2IC/src/Modelo/LecturaDatos.java
@@ -0,0 +1,50 @@
+package Modelo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LecturaDatos {
+
+	//TODO HACER CATCH
+    public static ArrayList<String> readAtributos(File fichero) throws IOException {
+    	FileReader lector = new FileReader(fichero);
+        BufferedReader buffer = new BufferedReader(lector);
+
+        String linea = buffer.readLine();
+        String[] aux = linea.split(",");
+        List<String> aux2 = Arrays.asList(aux);
+        ArrayList<String> atributos = new ArrayList<String>(aux2);
+       
+        buffer.close();
+        lector.close();
+        return atributos;
+    }
+    
+    //TODO HACER CATCH
+    public static ArrayList<ArrayList<String>> readEjemplos(File fichero) throws IOException {
+    	FileReader lector = new FileReader(fichero);
+        BufferedReader buffer = new BufferedReader(lector);
+        ArrayList<ArrayList<String>> ejemplos = new ArrayList<ArrayList<String>>();
+        String[] aux;
+        List<String> aux2;
+
+        String linea;
+        while ((linea = buffer.readLine()) != null) {
+        	aux = linea.split(",");
+        	aux2 = Arrays.asList(aux);
+        	ArrayList<String> aux3 = new ArrayList<String>(aux2);
+        	ejemplos.add(aux3);
+        }
+
+        buffer.close();
+        lector.close();
+        return ejemplos;
+    }
+}
+
+
